9208dc62e348fc7a1f5a264f01b9139ca19215e2
windSeal <564194070@qq.com>
2023-03-08 11:10:33 +0000

    添加关于exec替换文件的注解

--- watching/hookCreateFork.c
+++ watching/hookCreateFork.c
@@ -4,6 +4,12 @@
 
 #define ARGSIZE 128
 
+enum eventType
+{
+	EventArg,
+	EventRet,
+};
+
 // 抽象出来一个进程的描述信息
 struct forkInfo {
 	u32 pid;			// 用户空间的进程ID
@@ -12,6 +18,7 @@ struct forkInfo {
 	char comm[50];		// 进程名称
 	char argv[ARGSIZE];	// 创建一个数组用来接收插桩点参数的内容
 	int retval;			// 记录插桩点的返回值
+	enum eventType type;// 记录事件的类型
 }
 
 // 创建一个性能事件，在触发该事件的时时候发生前后端交互行为
@@ -69,7 +76,68 @@ int hookSyscallExecve(struct pt_regs *ctx, const char * filename, const char *co
 	// 创建结构体，获取当前进程的信息
 	struct task_struct *task;
 	
-	data.ppid = pid;
-	task = 
+	data.pid = pid;
+	// 辅助函数 获取任务(进程)的task结构体
+	task = (struct task_struct *)bpf_get_current_task();
+	// 从task里面拿父进程信息
+	data.ppid = task->real_parent->tgid;
+	
+	// 父进程过滤信息
+	PPID_FILTER
+	// 辅助函数，用来获取当前进程名称
+	bpf_get_current_comm(&data.comm, sizeof(data.comm));
+	data.type = EventArg;
 
+	//读取写入参数
+	__getArgs(ctx, (void*)filename, &data);
+	//跳过了第一个参数，现在读取剩下的参数  如果循环次数是常数，编译器会直接展开
+	#pragma unroll 
+	for (int i = 1; i < MAXARG; ++i) 
+	{
+		if (getArgs(ctx, (void*)&argv[i], &data) == 0) 
+		{
+			goto out;
+		}
+	}
+
+	// 如果参数太多，超过了最大限额，采取截断方式处理
+	char ellipsis[] = "...";
+	__getArgs(ctx, (void*)ellipis, &data)
+out:
+	return 0;
+}
+
+// 在离开函数的时候，上报下性能事件和外界交互
+int hookSyscallExecveRet(struct pt_regs* ctx)
+{
+	if (container_should_be_filtered())
+	{
+		return 0;
+	}
+
+    // 创建预设的数据结构，存储exec的参数并
+    struct forkInfo data = {};
+    // 创建结构体，获取当前进程的信息
+    struct task_struct *task;
+
+	u32 uid = bpf_get_current_uid_gid() & 0xffffffff;
+    UID_FILTER
+    data.pid = bpf_get_current_pid_tgid() >> 32;
+    data.uid = uid;
+
+    task = (struct task_struct *)bpf_get_current_task();
+	data.ppid = task->real_parent->tgid;
+
+	// 父进程ID过滤
+    PPID_FILTER
+	// 进程名获取
+    bpf_get_current_comm(&data.comm, sizeof(data.comm));
+    data.type = EVENT_RET;
+
+	// 获取返回值
+    data.retval = PT_REGS_RC(ctx);
+	//提交性能事件，开始和前端交互
+    events.perf_submit(ctx, &data, sizeof(data));
+    
+	return 0;
 }
